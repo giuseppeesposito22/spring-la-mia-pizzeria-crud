7eae28272674532c03b2007042fdbef030dcdcdc
giuseppeesposito22 <gius.espo22@gmail.com>
2025-09-03 17:56:39 +0200

    add show in pizza controller

--- src/main/java/org/lessons/java/spring_pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_pizzeria/controller/PizzaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,6 +23,14 @@ public class PizzaController {
         List<Pizza> pizzas = repo.findAll();
         model.addAttribute("pizzas", pizzas);
         return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id") Integer id){
+
+        Pizza pizza = repo.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "books/show";
     }
     
 }
